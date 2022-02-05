c245849cbb9eb648c4ecffc672fe9c298f6bffd4
Arai231456 <96648087+Arai231456@users.noreply.github.com>
2022-02-05 15:01:39 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+prompt("Which year are you in?");
+alert("912 days left until the freedom");
+
+function display()
+{
+  document.getElementById('date').innerHTML = Date();
+}
+
+function FunctionMultiply()
+
+ {
+  var firstNumBer = document.getElementById('number1').value;
+  var secondNumBer = document.getElementById('number2').value;
+  document.getElementById('result').innerHTML = (firstNumBer * secondNumBer);
+}
+
+function FunctionDivide()
+
+{
+  var firstNumBer = document.getElementById('number1').value;
+  var secondNumBer = document.getElementById('number2').value;
+  document.getElementById('result').innerHTML = (firstNumBer / secondNumBer);
+}
